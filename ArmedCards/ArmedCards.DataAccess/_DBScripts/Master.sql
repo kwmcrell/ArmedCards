6cbf89b04364c41aa3f6824b6d9f0e8485a2a93f
Kevin McRell <kwmcrell@gmail.com>
2013-09-27 23:06:00 -0500

    Progress on Play Round Milestone
    Closes #18
    
    Including multipick answers

--- ArmedCards/ArmedCards.DataAccess/_DBScripts/Master.sql
+++ ArmedCards/ArmedCards.DataAccess/_DBScripts/Master.sql
@@ -2569,6 +2569,62 @@ GO
 * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+IF OBJECT_ID('[dbo].[GamePlayerCard_Delete]') IS NOT NULL
+BEGIN 
+    DROP PROC [dbo].[GamePlayerCard_Delete] 
+END 
+GO
+
+-- ==============================================
+-- Author:		Kevin McRell
+-- Create date: 9/19/2013
+-- Description:	Delete GamePlayerCards
+-- ===============================================
+CREATE PROC [dbo].[GamePlayerCard_Delete]
+	@GameID			int,
+	@UserId			int,
+	@CardIDs		XML
+AS 
+	SET NOCOUNT ON 
+	SET XACT_ABORT ON  
+	
+	BEGIN TRAN
+	
+	DELETE
+	FROM [dbo].[GamePlayerCard]
+	WHERE [CardID] IN (SELECT ids.id.value('@value', 'int')
+					   FROM	 @CardIDs.nodes('ids/id') AS ids ( id ))
+	AND [GameID] = @GameID
+	AND [UserId] = @UserId
+
+	COMMIT
+GO
+
+GO 
+
+/*
+* Copyright (c) 2013, Kevin McRell & Paul Miller
+* All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification, are permitted
+* provided that the following conditions are met:
+* 
+* * Redistributions of source code must retain the above copyright notice, this list of conditions
+*   and the following disclaimer.
+* * Redistributions in binary form must reproduce the above copyright notice, this list of
+*   conditions and the following disclaimer in the documentation and/or other materials provided
+*   with the distribution.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+* WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 IF OBJECT_ID('[dbo].[GamePlayerCard_Select]') IS NOT NULL
 BEGIN 
     DROP PROC [dbo].[GamePlayerCard_Select] 
@@ -2581,7 +2637,8 @@ GO
 -- Description:	Select GamePlayerCards
 -- ===============================================
 CREATE PROC [dbo].[GamePlayerCard_Select]
-	@GameID			int
+	@GameID			int,
+	@UserId			int = NULL
 AS 
 	SET NOCOUNT ON 
 	SET XACT_ABORT ON  
@@ -2597,7 +2654,8 @@ AS
 			C.[CreatedBy_UserId]
 	FROM [dbo].[GamePlayerCard] GPC
 	INNER JOIN [dbo].[Card] C ON C.[CardID] = GPC.[CardID]
-	WHERE GPC.[GameID] = @GameID
+	WHERE	GPC.[GameID] = @GameID
+	AND		GPC.[UserId] = @UserId OR @UserId IS NULL
 
 	COMMIT
 GO
@@ -2802,6 +2860,81 @@ GO
 * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+IF OBJECT_ID('[dbo].[GameRound_SelectCurrent]') IS NOT NULL
+BEGIN 
+    DROP PROC [dbo].[GameRound_SelectCurrent] 
+END 
+GO
+
+-- ==============================================
+-- Author:		Kevin McRell
+-- Create date: 9/16/2013
+-- Description:	Selects current round based on game id
+-- ===============================================
+CREATE PROC [dbo].[GameRound_SelectCurrent] 
+	@GameID			int
+AS 
+	SET NOCOUNT ON 
+	SET XACT_ABORT ON  
+	
+	BEGIN TRAN
+
+	SELECT TOP 1	GR.[GameRoundID],
+					GR.[Started],
+					GR.[Game_GameID],
+					GR.[CardCommander_UserId] AS UserId,
+					UP.[UserName],
+					C.[CardID],
+					C.[Content],
+					C.[Instructions],
+					C.[Type],
+					C.[CreatedBy_UserId],
+					(
+						SELECT COUNT(DISTINCT GPC.[UserId])
+						FROM [dbo].[GamePlayerCard] GPC
+						WHERE GPC.[GameID] = @GameID
+						AND	  GPC.[UserId] <> GR.[CardCommander_UserId]
+					) AS CurrentPlayers,
+					(
+						SELECT COUNT(DISTINCT GRC2.[PlayedBy_UserId])
+						FROM [dbo].[GameRoundCard] GRC2
+						WHERE GRC2.[GameRound_GameRoundID] = GR.[GameRoundID]
+						AND	  GRC2.[PlayedBy_UserId] <> GR.[CardCommander_UserId]
+					) AS Played
+	FROM [dbo].[GameRound] GR
+	INNER JOIN [dbo].[UserProfile] UP ON UP.[UserId] = GR.[CardCommander_UserId]
+	INNER JOIN [dbo].[GameRoundCard] GRC ON GRC.[GameRound_GameRoundID] = GR.[GameRoundID]
+	INNER JOIN [dbo].[Card] C ON C.[CardID] = GRC.[Card_CardID] AND C.[Type] = 0
+	WHERE GR.[Game_GameID] = @GameID 
+	ORDER BY GR.[GameRoundID]
+
+	COMMIT
+GO
+GO 
+
+/*
+* Copyright (c) 2013, Kevin McRell & Paul Miller
+* All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification, are permitted
+* provided that the following conditions are met:
+* 
+* * Redistributions of source code must retain the above copyright notice, this list of conditions
+*   and the following disclaimer.
+* * Redistributions in binary form must reproduce the above copyright notice, this list of
+*   conditions and the following disclaimer in the documentation and/or other materials provided
+*   with the distribution.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+* WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 IF OBJECT_ID('[dbo].[GameRoundCard]') IS NULL
 	BEGIN
 
@@ -2845,3 +2978,69 @@ IF OBJECT_ID('[dbo].[GameRoundCard]') IS NULL
 
 	ALTER TABLE [dbo].[GameRoundCard] CHECK CONSTRAINT [FK_dbo.GameRoundCard_dbo.GameRound_GameRound_GameRoundID]
 END
+GO 
+
+/*
+* Copyright (c) 2013, Kevin McRell & Paul Miller
+* All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification, are permitted
+* provided that the following conditions are met:
+* 
+* * Redistributions of source code must retain the above copyright notice, this list of conditions
+*   and the following disclaimer.
+* * Redistributions in binary form must reproduce the above copyright notice, this list of
+*   conditions and the following disclaimer in the documentation and/or other materials provided
+*   with the distribution.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+* WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+IF OBJECT_ID('[dbo].[GameRoundCard_Select]') IS NOT NULL
+BEGIN 
+    DROP PROC [dbo].[GameRoundCard_Select] 
+END 
+GO
+
+-- ==============================================
+-- Author:		Kevin McRell
+-- Create date: 9/19/2013
+-- Description:	Select GameRoundCard
+-- ===============================================
+CREATE PROC [dbo].[GameRoundCard_Select]
+	@GameRoundID			int
+AS 
+	SET NOCOUNT ON 
+	SET XACT_ABORT ON  
+	
+	BEGIN TRAN
+
+	SELECT	GRC.[Card_CardID] AS CardID,
+			GRC.[DatePlayed],
+			GRC.[Game_GameID],
+			GRC.[GameRound_GameRoundID],
+			GRC.[GameRoundCardID],
+			GRC.[PlayedBy_UserId],
+			GRC.[PlayOrder],
+			GRC.[Winner],
+			C.[Content],
+			C.[Instructions],
+			C.[Type],
+			C.[CreatedBy_UserId],
+			UP.[UserId],
+			UP.[UserName]
+	FROM [dbo].[GameRoundCard] GRC
+	INNER JOIN [dbo].[Card] C ON C.[CardID] = GRC.[Card_CardID]
+	INNER JOIN [dbo].[UserProfile] UP ON GRC.[PlayedBy_UserId] = UP.[UserId]
+	WHERE	GRC.[GameRound_GameRoundID] = @GameRoundID
+
+	COMMIT
+GO
+
