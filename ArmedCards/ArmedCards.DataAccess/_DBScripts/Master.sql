1e254c50cbf4adeb03a3cbb6eecf2cd98b3a80ad
Kevin McRell <kwmcrell@gmail.com>
2013-10-03 00:13:17 -0500

    Generated new master.sql and fixed page titles

--- ArmedCards/ArmedCards.DataAccess/_DBScripts/Master.sql
+++ ArmedCards/ArmedCards.DataAccess/_DBScripts/Master.sql
@@ -576,8 +576,8 @@ AS
 											FROM [dbo].[GamePlayerCard] GPC
 											WHERE GPC.[CardID] = C.[CardID]
 										)
-								THEN 1
-							ELSE 0
+								THEN CAST(1 as BIT)
+							ELSE CAST(0 as BIT)
 						END
 					) AS CurrentlyInAHand
 	FROM [dbo].[Card] C
@@ -2436,6 +2436,10 @@ AS
 			WHERE [GameID] = @GameID AND [UserId] = @UserId
 
 		END
+		
+		DELETE
+		FROM [dbo].[GamePlayerCard]
+		WHERE [GameID] = @GameID AND [UserId] = @UserId
 
 	COMMIT
 GO
@@ -2854,6 +2858,60 @@ IF OBJECT_ID('[dbo].[GameRound]') IS NULL
 	ALTER TABLE [dbo].[GameRound] CHECK CONSTRAINT [FK_dbo.GameRound_dbo.Game_Game_GameID]
 
 	END
+GO 
+
+/*
+* Copyright (c) 2013, Kevin McRell & Paul Miller
+* All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification, are permitted
+* provided that the following conditions are met:
+* 
+* * Redistributions of source code must retain the above copyright notice, this list of conditions
+*   and the following disclaimer.
+* * Redistributions in binary form must reproduce the above copyright notice, this list of
+*   conditions and the following disclaimer in the documentation and/or other materials provided
+*   with the distribution.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+* WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+IF OBJECT_ID('[dbo].[GameRound_Delete]') IS NOT NULL
+BEGIN 
+    DROP PROC [dbo].[GameRound_Delete] 
+END 
+GO
+
+-- =====================================================
+-- Author:		Kevin McRell
+-- Create date: 10/02/2013
+-- Description:	Delete a round and all the cards played
+-- ======================================================
+CREATE PROC [dbo].[GameRound_Delete] 
+	@GameRoundID			int
+AS 
+	SET NOCOUNT ON 
+	SET XACT_ABORT ON  
+	
+	BEGIN TRAN
+
+	DELETE
+	FROM [dbo].[GameRoundCard]
+	WHERE [GameRound_GameRoundID] = @GameRoundID
+
+	DELETE
+	FROM [dbo].[GameRound]
+	WHERE [GameRoundID] = @GameRoundID
+
+	COMMIT
+GO
 GO 
 
 /*
