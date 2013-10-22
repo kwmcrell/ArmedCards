4225ebe4057cdb0fd6c784b0791d628ccfc5923b
Kevin McRell <kwmcrell@gmail.com>
2013-10-21 22:56:06 -0500

    Updated Master Sql file

--- ArmedCards/ArmedCards.DataAccess/_DBScripts/Master.sql
+++ ArmedCards/ArmedCards.DataAccess/_DBScripts/Master.sql
@@ -256,7 +256,8 @@ GO
 -- Description:	Creates a new User
 -- ===============================================
 CREATE PROC [dbo].[ActiveConnection_Select]
-	@GroupName				varchar(255)			  =	NULL
+	@GroupName				varchar(255)			  =	NULL,
+	@ExcludeUserIds			XML						  = NULL
 AS 
 	SET NOCOUNT ON 
 	SET XACT_ABORT ON  
@@ -270,6 +271,8 @@ AS
 	 FROM [dbo].[ActiveConnection] AC
 	 INNER JOIN [dbo].[UserProfile] UP ON UP.[UserId] = AC.[User_UserId]
 	 WHERE (AC.[GroupName] = @GroupName OR @GroupName IS NULL)
+	 AND   (@ExcludeUserIds IS NULL OR UP.UserId NOT IN (SELECT ids.id.value('@value', 'int')
+														 FROM	@ExcludeUserIds.nodes('ids/id') AS ids ( id )))
 
 	COMMIT
 GO 
@@ -396,10 +399,10 @@ GO
 -- ==============================================
 -- Author:		Kevin McRell
 -- Create date: 9/29/2013
--- Description:	Select a user profile
+-- Description:	Select user profiles
 -- ===============================================
 CREATE PROC [dbo].[User_Select] 
-    @UserId int
+    @UserIds XML
 AS 
 	SET NOCOUNT ON 
 	SET XACT_ABORT ON  
@@ -410,7 +413,8 @@ AS
 			UP.[UserName],
 			UP.[PictureUrl]
 	FROM [dbo].[UserProfile] UP
-	WHERE UP.[UserId] = @UserId
+	WHERE UP.[UserId] IN (SELECT ids.id.value('@value', 'int')
+							FROM	@UserIds.nodes('ids/id') AS ids ( id ))
 
 	COMMIT
 GO
@@ -3599,7 +3603,7 @@ AS
 	AND GPKV.[Vote] = 0
 	
 	IF NOT EXISTS	(	
-						SELECT GPKV.[VotedUserId]
+						SELECT TOP 1 GPKV.[VotedUserId]
 						FROM [GamePlayerKickVote] GPKV
 						WHERE GPKV.[GameID] = @GameID 
 						AND GPKV.[KickUserId] = @KickUserId
@@ -3619,6 +3623,11 @@ AS
 				@VotedUserId,
 				@Vote
 		END
+
+	SELECT GPKV.[VotedUserId]
+	FROM [GamePlayerKickVote] GPKV
+	WHERE GPKV.[GameID] = @GameID 
+	AND GPKV.[KickUserId] = @KickUserId
 
 	COMMIT TRAN
 		
@@ -3680,6 +3689,65 @@ AS
 	FROM [GamePlayerKickVote] GPKV
 	WHERE GPKV.[GameID] = @GameID 
 	AND GPKV.[KickUserId] = @KickUserId
+
+	DELETE
+	FROM [GamePlayerKickVote]
+	WHERE [GameID] = @GameID 
+	AND [KickUserId] = @KickUserId
+
+	COMMIT TRAN
+		
+GO
+GO 
+
+/*
+* Copyright (c) 2013, Kevin McRell & Paul Miller
+* All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification, are permitted
+* provided that the following conditions are met:
+* 
+* * Redistributions of source code must retain the above copyright notice, this list of conditions
+*   and the following disclaimer.
+* * Redistributions in binary form must reproduce the above copyright notice, this list of
+*   conditions and the following disclaimer in the documentation and/or other materials provided
+*   with the distribution.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+* WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+IF OBJECT_ID('[dbo].[GamePlayerKickVote_SelectForGame]') IS NOT NULL
+BEGIN 
+    DROP PROC [dbo].[GamePlayerKickVote_SelectForGame] 
+END 
+GO
+
+-- ==============================================
+-- Author:		Kevin McRell
+-- Create date: 10/19/2013
+-- Description:	Select all votes to kick users in a game
+-- ===============================================
+CREATE PROC [dbo].[GamePlayerKickVote_SelectForGame] 
+	@GameID			int
+AS 
+	SET NOCOUNT ON 
+	SET XACT_ABORT ON  
+	
+	BEGIN TRAN 
+
+	SELECT	GPKV.[GameID],
+			GPKV.[KickUserId],
+			GPKV.[Vote],
+			GPKV.[VotedUserId]
+	FROM [GamePlayerKickVote] GPKV
+	WHERE GPKV.[GameID] = @GameID
 
 	COMMIT TRAN
 		
